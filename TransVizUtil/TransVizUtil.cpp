032d0d3e58e6387b931a9f76fdb23dbe0a97e97a
Naman <namangupta@vizexperts.com>
2018-10-22 18:28:34 +0530

    Updated TransVizUtil

--- TransVizUtil/TransVizUtil.cpp
+++ TransVizUtil/TransVizUtil.cpp
@@ -2,7 +2,40 @@
 #include "scenegraphspu.h"
 #include <cr_server.h>
 
+#include <osgDB/Export>
+#include <osgDB/Registry>
+#include <osgDB/ReadFile>
+#include <osgDB/Writefile>
+
 namespace TransVizUtil{
+
+	class TransVizUtilCallback : public osg::NodeCallback {
+
+		TransVizUtil* _util;
+
+	public:
+
+		TransVizUtilCallback(TransVizUtil* util)
+		{
+			_util = util;
+		}
+
+		void operator()(osg::Node* node, osg::NodeVisitor* nv) {
+
+			if ((GetKeyState('A') & 0x8000) && (GetKeyState(VK_LMENU) & 0x8000))
+			{
+				getUpdatedScene();
+			}
+
+			if ((GetKeyState('Y') & 0x8000) && (GetKeyState(VK_LMENU) & 0x8000))
+			{
+				osgDB::writeNodeFile( *(_util->getLastGeneratedNode().get()) , "saved.ive");
+			}
+
+			traverse(node, nv);
+		}
+
+	};
 
 	void forwarder(void* context, osg::ref_ptr<osg::Group> node) {
 		static_cast<TransVizUtil*>(context)->updateNode(node);
@@ -11,7 +44,9 @@ namespace TransVizUtil{
     TransVizUtil::TransVizUtil() :
         _rootNode(NULL),
         _thread(NULL),
-		_oldNode(NULL)
+		_oldNode(NULL),
+		_newNode(NULL),
+		_bIsNodeDirty(false)
     {
     }
 
@@ -19,6 +54,10 @@ namespace TransVizUtil{
 
     void TransVizUtil::setRootNode(osg::Group* root){
         _rootNode = root;
+
+		osg::Group* btnCBnode = new osg::Group;
+		btnCBnode->addUpdateCallback(new TransVizUtilCallback(this));
+		_rootNode->addChild(btnCBnode);
     }
 
 	osg::ref_ptr<osg::Group> TransVizUtil::getRootNode(){
@@ -33,18 +72,30 @@ namespace TransVizUtil{
 		getUpdatedScene();
 	}
 
-	void TransVizUtil::updateNode(osg::ref_ptr<osg::Group> node)
+	void TransVizUtil::update()
 	{
-		osg::Group* newGroup = node->asGroup();
+		if (!_bIsNodeDirty)
+			return;
+
+		osg::Group* newGroup = _newNode->asGroup();
 		if (newGroup != NULL && _rootNode != NULL){
-			if (_oldNode != NULL) {
-				_rootNode->replaceChild(_oldNode, newGroup);
+			if (_oldNode != NULL && _oldNode != newGroup) {
+				_rootNode->removeChild(_oldNode);
+				_rootNode->addChild(newGroup);
 			}
 			else {
 				_rootNode->addChild(newGroup);
 			}
 			_oldNode = newGroup;
 		}
+
+		_bIsNodeDirty = false;
+	}
+
+	void TransVizUtil::updateNode(osg::ref_ptr<osg::Group> node)
+	{
+		_newNode = node;
+		_bIsNodeDirty = true;
 	}
 
 	void TransVizUtil::run(int argc, char* argv[]) {
@@ -66,7 +117,15 @@ namespace TransVizUtil{
 
     void CRServerThread::run()
     {
-        CRServerMain(_argc, _argv);
+		crServerInit(_argc, _argv);
+
+		SPU* spu = crServerHeadSPU();
+		//IFunc* fun = spu->privatePtr;
+
+		while (1)
+		{
+			crServerServiceClients();
+		}
     }
 
     int CRServerThread::cancel()
