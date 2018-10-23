bd2c48229443b8df3e0037b7439ff003217ef766
Naman <namangupta@vizexperts.com>
2018-10-23 18:30:07 +0530

    fetch deps fix

--- FetchExternalDeps.bat
+++ FetchExternalDeps.bat
@@ -2,6 +2,6 @@
 # executing this command
 
 svn co http://192.168.0.2:18080/svn/OSS_ADA/trunk/glut/ ExternalLibrary/glut
-svn co http://192.168.0.2:18080/svn/OSS_ADA/trunk/OSG/3.4.2 ExternalLibrary/osg-3.4
+svn co http://192.168.0.2:18080/svn/OSS_ADA/trunk/osg-3.4/ ExternalLibrary/osg-3.4
 svn co http://192.168.0.2:18080/svn/OSS_ADA/trunk/flexera ExternalLibrary/flexera
 svn co http://192.168.0.2:18080/svn/OSS_ADA/trunk/libjpeg-turbo64/ ExternalLibrary/libjpeg-turbo64/
