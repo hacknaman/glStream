ec5c6a8da34644cfc0eb40306c2042038476b785
Naman <namangupta@vizexperts.com>
2018-10-25 15:26:20 +0530

    release and debug aveva spu library

--- ExternalLibrary/FindReviewAPI.cmake
+++ ExternalLibrary/FindReviewAPI.cmake
@@ -9,13 +9,17 @@ FIND_PATH(REVIEW_INCLUDE_DIR  ReviewCpp.h
 		NO_DEFAULT_PATH    
 )
 
-# only release library
 find_library(REVIEW_LIBRARY 
 		NAMES ReviewCppApi
 		PATHS $ENV{REVIEW_DIR}/lib/
 		NO_DEFAULT_PATH    
 )
 
+find_library(REVIEW_LIBRARY_D 
+		NAMES ReviewCppApid
+		PATHS $ENV{REVIEW_DIR}/lib/
+		NO_DEFAULT_PATH    
+)
 
 SET(REVIEW_FOUND "NO")
 IF(REVIEW_LIBRARY AND REVIEW_INCLUDE_DIR )
