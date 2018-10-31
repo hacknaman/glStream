1c7c32409f6dd440f737ac2ba7646c041400e632
Lokesh Sharma <lokesh.sharma@vizexperts.com>
2018-10-31 15:33:27 +0530

    teamcity config

---
+++ teamcityBuild.bat
@@ -0,0 +1,35 @@
+:: OSS ROOT PATH IS SET FROM TEAMCITY CLIENT
+
+:: set OSG Directories
+set OSG_DIR=%OSSROOT%\osg-3.4
+set OSG_BIN=%OSG_DIR%\bin
+set OSG_PLUGIN_DIR=%OSG_BIN%\osgPlugins-3.4.2
+
+:: set flexera libraries
+set FLEXERA_DIR=%OSSROOT%\flexera\11.12
+set FLEXERA_BIN=%FLEXERA_DIR%\bin
+
+::set GLUT Libraries
+set GLUT_DIR=%OSSROOT%\glut
+set GLUT_BIN=%GLUT_DIR%\bin\x64
+
+:: set libjpeg libraries
+set LIBJPEG_DIR=%OSSROOT%\libjpeg-turbo64
+set LIBJPEG_BIN=%LIBJPEG_DIR%\bin
+
+:: set Review API libraries
+set REVIEW_DIR=%OSSROOT%\ReviewApi
+set REVIEW_BIN=%REVIEW_DIR%\bin
+
+:: add external DLLs to the PATH
+:: dynamic libs paths
+set PATH=%OSG_BIN%;%OSG_PLUGIN_DIR%;%FLEXERA_BIN%;%GLUT_BIN%;%LIBJPEG_BIN%;%REVIEW_BIN%;%PATH%
+
+:: set development mode
+:: This is used for licensing
+set Development=false
+unset DBG
+unset RLS
+
+call GenerateSolution.bat
+cd ..
