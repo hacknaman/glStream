fecff3378372c3c917e23c17e2b7de5898def6da
VizExperts India Pvt Ltd <gitlab@vizexperts.com>
2019-01-14 04:10:15 +0000

    Update GenerateSolution.bat

--- GenerateSolution.bat
+++ GenerateSolution.bat
@@ -1,9 +1,9 @@
 :: cmake should be added to the path before 
 :: executing this command
 
-mkdir x64
-cd x64
-
+set FolderName=x64
+if not exist %FolderName% mkdir %FolderName%
+cd %FolderName%
 If exist CMakeCache.txt del CMakeCache.txt
-
 cmake -G "Visual Studio 12 2013 Win64" ../build ..
+
