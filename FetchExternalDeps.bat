27ca367de2e8d536fcc97076fda1045c4c3add80
VizExperts India Pvt Ltd <gitlab@vizexperts.com>
2019-02-28 07:04:27 +0000

    Update FetchExternalDeps.bat (commenting comments)

--- FetchExternalDeps.bat
+++ FetchExternalDeps.bat
@@ -1,5 +1,5 @@
-# svn client should be added to the path before 
-# executing this command
+::# svn client should be added to the path before 
+::# executing this command
 
 svn co http://192.168.0.2:18080/svn/OSS_ADA/trunk/glut/ 	ExternalLibrary/glut
 svn co http://192.168.0.2:18080/svn/OSS_ADA/trunk/osg-3.4/ 	ExternalLibrary/osg-3.4
