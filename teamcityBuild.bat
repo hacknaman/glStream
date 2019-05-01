9b5686815facf3da5d6844c153435236f3388d39
gaurav <gauravsharma@vizexperts.com>
2019-05-01 19:24:42 +0530

    teamcity.bat modified

--- teamcityBuild.bat
+++ teamcityBuild.bat
@@ -1,36 +1,43 @@
 :: OSS ROOT PATH IS SET FROM TEAMCITY CLIENT
 
 call FetchExternalDeps.bat
-set OSSROOT=%cd%\ExternalLibrary
+set OSSROOT=%cd%/ExternalLibrary
+set "OSSROOT=%OSSROOT:\=/%"
 
+:: SET OSS ROOT PATH
 :: set OSG Directories
-set OSG_DIR=%OSSROOT%\osg-3.4
-set OSG_BIN=%OSG_DIR%\bin
-set OSG_PLUGIN_DIR=%OSG_BIN%\osgPlugins-3.4.2
+set OSG_DIR=%OSSROOT%/osg-3.4
+set OSG_BIN=%OSG_DIR%/bin/%BUILD_ARCH%
 
 :: set flexera libraries
-set FLEXERA_DIR=%OSSROOT%\flexera\11.12
-set FLEXERA_BIN=%FLEXERA_DIR%\bin
+set FLEXERA_DIR=%OSSROOT%/flexera/11.12
+set FLEXERA_BIN=%FLEXERA_DIR%/bin/%BUILD_ARCH%
+
+::set ServerContent Api libraries
+set SERVER_CONTENT_API_DIR=%OSSROOT%/ServerAppContentApi
+set SERVER_CONTENT_API_BIN = %SERVER_CONTENT_API_DIR%/bin/%BUILD_ARCH%
+if not "%BUILD_ARCH%"=="x86" goto set64BitEnv 
+
+:: set OSG Plugin Directories
+set OSG_PLUGIN_DIR=%OSG_BIN%/osgPlugins-3.7.0
 
 ::set GLUT Libraries
-set GLUT_DIR=%OSSROOT%\glut
-set GLUT_BIN=%GLUT_DIR%\bin\x64
+set GLUT_DIR=%OSSROOT%/glut
+set GLUT_BIN=%GLUT_DIR%/bin
+goto end
 
-:: set libjpeg libraries
-set LIBJPEG_DIR=%OSSROOT%\libjpeg-turbo64
-set LIBJPEG_BIN=%LIBJPEG_DIR%\bin
+:set64BitEnv
+set OSG_PLUGIN_DIR=%OSG_BIN%osgPlugins-3.4.2
 
-:: set Review API libraries
-set REVIEW_DIR=%OSSROOT%\ReviewApi
-set REVIEW_BIN=%REVIEW_DIR%\bin
-
-:: set Catia API libraries
-set CatiaCppApi_DIR=%OSSROOT%\CatiaCppApi
-set CatiaCppApi_BIN=%CatiaCppApi_DIR%\bin
+::set GLUT Libraries
+set GLUT_DIR=%OSSROOT%/glut
+set GLUT_BIN=%GLUT_DIR%/bin\x64
+goto end
+:end
 
 :: add external DLLs to the PATH
 :: dynamic libs paths
-set PATH=%OSG_BIN%;%OSG_PLUGIN_DIR%;%FLEXERA_BIN%;%GLUT_BIN%;%LIBJPEG_BIN%;%REVIEW_BIN%;%CatiaCppApi_BIN%;%PATH%
+set PATH=%OSG_BIN%;%OSG_PLUGIN_DIR%;%FLEXERA_BIN%;%GLUT_BIN%;%LIBJPEG_BIN%;%SERVER_CONTENT_API_BIN%;%PATH%
 
 echo "================NEW BUILD==============="
 del "x64\TransViz.sln"
