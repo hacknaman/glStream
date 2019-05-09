269fbc3c6ece2baaf74000fee113a8e36542edbc
Naman Gupta <hacknaman@gmail.com>
2019-05-09 11:26:35 +0530

    removed deprecated files

--- FetchExternalDeps.bat
+++ FetchExternalDeps.bat
@@ -1,9 +1,7 @@
 ::svn client should be added to the path before 
 ::executing this command
 
-svn co http://192.168.0.2:18080/svn/OSS_ADA/trunk/glut/ 	ExternalLibrary/glut
-svn co http://192.168.0.2:18080/svn/OSS_ADA/trunk/osg-3.4/ 	ExternalLibrary/osg-3.4
-svn co http://192.168.0.2:18080/svn/OSS_ADA/trunk/flexera 	ExternalLibrary/flexera
-svn co http://192.168.0.2:18080/svn/OSS_ADA/trunk/ReviewApi/ 		ExternalLibrary/ReviewApi/
-svn co http://192.168.0.2:18080/svn/OSS_ADA/trunk/CatiaCppApi/		ExternalLibrary/CatiaCppApi/
+svn co http://192.168.0.2:18080/svn/OSS_ADA/trunk/glut/ 	                ExternalLibrary/glut
+svn co http://192.168.0.2:18080/svn/OSS_ADA/trunk/osg-3.4/ 	                ExternalLibrary/osg-3.4
+svn co http://192.168.0.2:18080/svn/OSS_ADA/trunk/flexera 	                ExternalLibrary/flexera
 svn co http://192.168.0.2:18080/svn/OSS_ADA/trunk/ServerAppContentApi/		ExternalLibrary/ServerAppContentApi/
